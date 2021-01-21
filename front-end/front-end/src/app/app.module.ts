0e1169b3cdf722707ac674ec52ab53179addb49e
Joaquim Silva <joaquim.diego8@gmail.com>
2021-01-21 16:41:28 -0300

    login-cadastro

--- front-end/front-end/src/app/app.module.ts
+++ front-end/front-end/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginCadastroComponent } from './login-cadastro/login-cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginCadastroComponent
   ],
   imports: [
     BrowserModule,
