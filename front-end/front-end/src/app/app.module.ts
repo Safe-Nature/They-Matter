04fd3f3ecf69cdd2478181276bee9678823e88c4
Leon-Moraes <leo_o_nardo1@outlook.com>
2021-01-21 16:24:50 -0300

    Implementação da main-page 0.1

--- front-end/front-end/src/app/app.module.ts
+++ front-end/front-end/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
